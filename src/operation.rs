0e6d0a4d0d97204610f762ab40a62c5bc5117d4f
Chin Zhe Ning <ning.chin03@gmail.com>
2025-11-26 13:45:57 +0800

    add: division by zero results in math error

--- src/operation.rs
+++ src/operation.rs
@@ -1,7 +1,11 @@
 #[derive(Debug, Clone, PartialEq)]
 pub enum Operation {
     Const(f64),
+
+    // Unary Operations
     Negate,
+    
+    // Binary Operations
     Add,
     Subtract,
     Times,
